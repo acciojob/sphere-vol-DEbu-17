b4e2d977a50f593ff8ee9cceb9ad243133421350
DEbu-17 <98536414+DEbu-17@users.noreply.github.com>
2023-10-22 10:10:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let radius = document.getElementById("radius");
+let volume = document.getElementById("volume");
+let r = radius.value;
+
 function volume_sphere() {
     //Write your code here
-  
+  return (4/3)*Math.PI*r*r*r;
 } 
 
+
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+volume.value = volume_sphere();
